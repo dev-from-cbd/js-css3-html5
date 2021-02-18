a115b236c1812182eaad6a1aab01c4ddecfc6785
veryfatwombat <veryfatwombat@gmail.com>
2021-02-18 13:04:11 +1100

    css

--- js/main.js
+++ js/main.js
@@ -1,12 +1,42 @@
+const menu = document.querySelector(".menu");
+const navOpen = document.querySelector(".hamburger");
+const navClose = document.querySelector(".close");
+const navBar = document.querySelector(".nav");
+
+const navLeft = menu.getBoundingClientRect().left;
+navOpen.addEventListener("click", ()=>{
+    if(navLeft < 0){
+        menu.classList.add("show");
+        document.body.classList.add("show");
+        navBar.classList.add("show");
+    }
+});
+
+navClose.addEventListener("click", () => {
+    if(navLeft < 0) {
+        menu.classList.remove("show");
+        document.body.classList.remove("show");
+        navBar.classList.remove("show");
+    }
+});
+
 new TypeIt("#type1", {
     speed: 120,
     loop: true,
     waitUntilVisible: true,
 })
-    .type("Designer", { delay: 400 })
+    .type("a Barber", { delay: 400 })
     .pause(500)
     .delete(9)
-    .type("Developer", { delay: 400 })
+    .type("an Expert", { delay: 400 })
     .pause(500)
     .delete(9)
     .go();
+
+gsap.from(".logo", {opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".hamburger", {opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".logo", {opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".logo", {opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".logo", {opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".logo", {opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".logo", {opacity: 0, duration: 1, delay: 0.5, y: -10 });
