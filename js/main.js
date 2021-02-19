00694c2807e94f8c395b94809158e3867e54379e
veryfatwombat <veryfatwombat@gmail.com>
2021-02-19 11:21:46 +1100

    Javascript

--- js/main.js
+++ js/main.js
@@ -33,10 +33,40 @@ new TypeIt("#type1", {
     .delete(9)
     .go();
 
+    new TypeIt("#type2", {
+        speed: 120,
+        loop: true,
+        waitUntilVisible: true,
+    })
+        .type("a Barber", { delay: 400 })
+        .pause(500)
+        .delete(9)
+        .type("an Expert", { delay: 400 })
+        .pause(500)
+        .delete(9)
+        .go();
+    
 gsap.from(".logo", {opacity: 0, duration: 1, delay: 0.5, y: -10 });
-gsap.from(".hamburger", {opacity: 0, duration: 1, delay: 0.5, y: -10 });
-gsap.from(".logo", {opacity: 0, duration: 1, delay: 0.5, y: -10 });
-gsap.from(".logo", {opacity: 0, duration: 1, delay: 0.5, y: -10 });
-gsap.from(".logo", {opacity: 0, duration: 1, delay: 0.5, y: -10 });
-gsap.from(".logo", {opacity: 0, duration: 1, delay: 0.5, y: -10 });
-gsap.from(".logo", {opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".hamburger", {opacity: 0, duration: 1, delay: 1, x: 20 });
+gsap.from(".banner", {opacity: 0, duration: 1, delay: 1.5, x: -200 });
+gsap.from(".hero h3", {opacity: 0, duration: 1, delay: 2, y: -50 });
+gsap.from(".hero h2", {opacity: 0, duration: 1, delay: 2.5, y: -45 });
+gsap.from(".hero h4", {opacity: 0, duration: 1, delay: 3, y: -30 });
+gsap.from(".hero a", {opacity: 0, duration: 1, delay: 3.5, y: 50 });
+gsap.from(".nav-item", {
+    opacity: 0, 
+    duration: 1, 
+    delay: 1.2, 
+    y: 30, 
+    stagger: 0.2, 
+});
+
+gsap.from(".icon span", {
+    opacity: 0, 
+    duration: 1, 
+    delay: 4, 
+    x: -30, 
+    stagger: 0.2, 
+});
+
+
